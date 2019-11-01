3041ab268345ba4bb91a454c59685c24f9560852
stravze <stravze@gmail.com>
2019-11-01 14:59:02 +0000

    updating missing variables

--- 01-inputs-required.tf
+++ 01-inputs-required.tf
@@ -33,3 +33,35 @@ variable "subnet-mgmt-prefix" {
   description                       = "Management IP Range Prefix"
   
 }
+
+
+
+variable "environment" { }
+
+
+
+variable "firewall_name_prefix" {
+  default                     = "core-dmz-fw"
+}
+
+variable "firewall_username" {
+  description                 = "Username to provision the VM with"
+}
+
+variable "firewall_password" {
+  description                 = "Password to provision the VM with"
+}
+
+variable "firewall_subnet_management" {
+  description                 = "Name of the subnet to be used for the firewall management nic"
+}
+
+variable "firewall_subnet_transit_private" {
+  description                 = "Name of the subnet to be used for the firewall transit nic"
+}
+
+variable "firewall_subnet_transit_public" { }
+
+variable "firewall_replicas" {
+  default                             = 1
+}
